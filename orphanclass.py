5f4ef465966180ed2c6ab9eae193eec3b4f86387
noahcordova15 <noahcordova@amdg.rockhursths.edu>
2020-03-03 08:47:14 -0600

    stereotype

---
+++ orphanclass.py
@@ -0,0 +1,7 @@
+ccclass orphan(object):
+    race = ["black", "hispanic", "white", "asian", "
+    skinc = ["black", "light black", "brown", "light brown", "tan white", "white"]
+    dsize = ["big", "average", "micro"]
+    ilevel = ["genius", "smart", "average", "dumb", "mentally handicapped"]
+
+    def __init__(self, skinc, color)
