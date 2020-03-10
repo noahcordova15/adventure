093af9ec9ae6a534adda2665e96d5173ced4bd31
noahcordova15 <noahcordova@amdg.rockhursths.edu>
2020-03-10 08:50:19 -0500

    y

--- game.intro.py
+++ game.intro.py
@@ -1,13 +1,29 @@
-from orphan import orphan
+#from orphan import orphan
 
 def intro():
     global name
-    name = input("Who is this child? ")
+    name = input("Who is this child? (Input name) ")
 
 def claim():
     global name
     print("Welcome to the family, " + name + " Skywalker.")
 
+def start():
+    global ptype
+    global name
+    print(name+", you have been adopted by an interesting family. You will require a certain set of skills; you will use these skills to complete tasks that build towards an inherently good or evil goal.")
+    choice = input("Now, choose, would you like to be good or evil? (type G or E)")
+    if(choice in ['G', 'g']):
+          ptype = 1
+          print("Congratulations, you are good")
+    elif(choice in ['E', 'e']):
+          ptype = 2
+          print("Congratulations, you are evil")
+    else:
+          print("Invalid input. Please try again.")
+    
+
 #driver
 intro()
 claim()
+start()
